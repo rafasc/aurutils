92f23f3e1563b6f0dff247ac251a5d5ed50e8c39
Rafael Ascensão <rafa.almas@gmail.com>
2020-08-27 21:42:31 +0100

    completions: bypass root warning when installing
    aurutils scripts have a molly-guard that prevents running them as root.
    
    However, the completion generation system needs to call each script with
    `--dump-options` to get the list of options to be completed.
    
    To allow installing aur and its completions as root, we disable the
    molly-guard setting `AUR_ASROOT=1` on each call.

--- completions/command_opts.sh
+++ completions/command_opts.sh
@@ -8,7 +8,7 @@ default_opts() {
     local cmd corecommands=() opts=()
 
     for cmd in "${have_optdump[@]}"; do
-        mapfile -t opts < <(bash "../lib/aur-${cmd}" --dump-options | LC_ALL=C sort)
+        mapfile -t opts < <(AUR_ASROOT=1 bash "../lib/aur-${cmd}" --dump-options | LC_ALL=C sort)
         corecommands+=("default_cmds[${cmd}]='${opts[*]}'")
     done
 
